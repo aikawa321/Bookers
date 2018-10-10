28b35d7417c10130836fc3ee8ab59ea2532168fa
aikawa321 <42262654+aikawa321@users.noreply.github.com>
2018-10-10 15:13:28 +0900

    [fix]4-8

--- Bookers/config/routes.rb
+++ Bookers/config/routes.rb
@@ -6,10 +6,8 @@ Rails.application.routes.draw do
   post 'books' => 'books#create'
   get '/books'=> 'books#index'
   get '/books/:id/show'=> 'books#show', as: 'show_book'
-   get '/books/:id/result'=> 'books#result', as:'result_book'
   get '/bookd/:id/edit'=>'books#edit'
   patch '/books/:id' => 'books#update', as:'update_book'
-  get '/books/:id/effect'=> 'books#effect', as:'effect_book'
 delete'/books/:id' =>'books#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
