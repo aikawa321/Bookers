0a319012854e39008efa12a381be25e919fcfde2
aikawa321 <42262654+aikawa321@users.noreply.github.com>
2018-10-18 16:07:34 +0900

    [Finish]5-9

---
+++ Bookers2/app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  def index
+  	@book = Book.new
+    @user = User.new
+    @user = User.find_by(id: current_user.id)
+  	@users = User.all
+  end
+  def show
+    @books = Book.find_by(id: current_user.id)
+  	@user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+  end
+
+  def edit
+    @user = User.find_by(id: current_user.id)
+  end
+
+  def update
+  	@user = User.find(params[:id])
+  if	@user.update(user_params)
+  	redirect_to user_path(current_user.id), notice: "updated successfully"
+  end
+  render :edit
+end
+  	private
+  	def user_params
+  		params.require(:user).permit(:name, :introduction, :image)
+    end
+end
