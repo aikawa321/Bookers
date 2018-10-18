6d434bd5b7f7e26e24146d7f358356d1b4c04aa9
aikawa321 <42262654+aikawa321@users.noreply.github.com>
2018-10-18 16:27:23 +0900

    [Finish]5-8

--- Bookers/app/controllers/books_controller.rb
+++ Bookers/app/controllers/books_controller.rb
@@ -19,7 +19,7 @@ class BooksController < ApplicationController
   def create
      @book = Book.new(book_params)
     if @book.save
-      flash[:notice] = 'Book was successflly created.'
+      flash[:notice] = 'Book was successfully created.'
     redirect_to book_path(@book.id)
     else
       @books = Book.all
@@ -34,7 +34,7 @@ class BooksController < ApplicationController
   def update
         book = Book.find(params[:id])
        if book.update(book_params)
-        flash[:notice] = 'Book was successflly updated.'
+        flash[:notice] = 'Book was successfully updated.'
       end
         redirect_to book_path(book.id)
   end
@@ -46,7 +46,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
    if book.destroy
-    flash[:notice] = 'Book was successflly destroyed.'
+    flash[:notice] = 'Book was successfully destroyed.'
     end
     redirect_to books_new_path
   end
