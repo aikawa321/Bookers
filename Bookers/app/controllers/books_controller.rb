ab3b38d909c753fc7eb3e36a7450d8a933dc7b5f
aikawa321 <42262654+aikawa321@users.noreply.github.com>
2018-10-23 16:55:35 +0900

    [fix]5-8Bookers

---
+++ Bookers/app/controllers/books_controller.rb
@@ -0,0 +1,59 @@
+class BooksController < ApplicationController
+  def show
+    @book = Book.find(params[:id])
+  end
+  def result
+     @book = Book.find(params[:id])
+  end
+
+  def index
+   
+  end
+
+  def new
+    # 空のモデルをビューへ渡す
+    @book = Book.new
+     @books = Book.all.reverse_order
+  end
+
+  def create
+     @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = 'Book was successfully created.'
+    redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render action: :new
+
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+  def update
+        book = Book.find(params[:id])
+       if book.update(book_params)
+        flash[:notice] = 'Book was successfully updated.'
+      end
+        redirect_to book_path(book.id)
+  end
+
+  def effect
+     @book = Book.find(params[:id])
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+   if book.destroy
+    flash[:notice] = 'Book was successfully destroyed.'
+    end
+    redirect_to books_new_path
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title,:body)
+  end
+
+end
