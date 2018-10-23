f2dfb4ecd5574f474c125d912f52a0d9d6f2a9fc
aikawa321 <guilty.gate0619@gmail.com>
2018-10-23 07:26:03 +0000

    [delete]bookers2

--- Bookers2/app/models/book.rb
+++
@@ -1,5 +0,0 @@
-class Book < ApplicationRecord
-	belongs_to :user
-	validates :opinion,length:{maximum:200}, presence: true
-	validates :title, presence: true
-end
--- Bookers2/config/initializers/application_controller_renderer.rb
+++
@@ -1,9 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# ActiveSupport::Reloader.to_prepare do
-#   ApplicationController.renderer.defaults.merge!(
-#     http_host: 'example.org',
-#     https: false
-#   )
-# end
-Refile.secret_key = '9f9ee489b9e04ecbe753fc00ba6a5a9f1b8754dc5c5aa93000b309aa65532fbf73090f12fa70d8d2d544a7828cf92017111316018d1cb4c8eed600313673982d'
